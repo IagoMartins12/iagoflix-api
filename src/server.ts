8c8b7f1d1931eccc88171722614e6b06f772b8e0
Iago saraiva <iago.saraiva@pmovil.com.br>
2023-04-12 09:22:12 -0300

    Deploy features

--- src/server.ts
+++ src/server.ts
@@ -20,7 +20,7 @@ app.use(cors())
 
 app.use(router)
 
-const PORT = process.env.port || 3000
+const PORT = process.env.PORT ||  3000
 
 app.listen(PORT, () => {
   database.authenticate().then(() => {
@@ -30,3 +30,4 @@ app.listen(PORT, () => {
   console.log(`Server started successfuly at port ${PORT}.`)
 })
 
+module.exports = app
